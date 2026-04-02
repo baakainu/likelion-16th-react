ad5ed4c07a194811b6a5adc74f568467ff0b01ac
baakainu <iwlamb9363@gmail.com>
2026-04-02 10:10:21 +0900

    add. 야무쌤 Github 학습 자료 추가

---
+++ learning-next-js/02-렌더링-바운더리/03-서버-클라이언트-인터리빙/src/app/page.tsx
@@ -0,0 +1,42 @@
+import { ClientComponent, ServerComponent } from '@/_learn'
+import { cn } from '@/utils'
+
+export default function Page() {
+
+  // 서버 컴포넌트 또는 클라이언트 컴포넌트에 서로를 포함해보세요.
+  // 어떤 일이 일어나는지 확인하고, 문제가 발생한다면 해결 방법도 살펴봅시다.
+
+  return (
+    <div
+      className={cn(
+        'flex flex-col items-center justify-center gap-5',
+        'bg-background min-h-screen',
+      )}
+    >
+      <header>
+        <h1
+          className={cn(
+            'text-foreground text-center text-4xl font-extralight',
+            'selection:bg-foreground selection:text-background',
+          )}
+        >
+          인터리빙
+          <br />
+          <span
+            lang="en"
+            className="inline-block -translate-y-2.5 text-xl text-slate-500"
+          >
+            Interleaving
+          </span>
+        </h1>
+      </header>
+
+      <main className="flex flex-col gap-5">
+        <ClientComponent>
+          {/* <slot></slot> <- props.children */}
+          <ServerComponent />
+        </ClientComponent>
+      </main>
+    </div>
+  )
+}
